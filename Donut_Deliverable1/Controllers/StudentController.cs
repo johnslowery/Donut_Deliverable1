396a345f278519d1aa26f5baaf55d3b84e2e84e0
JohnSLowery <lowery.jack20@gmail.com>
2020-09-30 19:00:48 -0400

    Search Bar Added
    It should add a search bar that can search through the student database by several different search criteria

---
+++ Donut_Deliverable1/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Donut_Deliverable1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Donut_Deliverable1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private IStudentRepository repository;
+        private readonly AppDbContext context;
+
+        public StudentController(AppDbContext context, IStudentRepository repo)
+        {
+            this.context = context;
+            repository = repo;
+        }
+        public IActionResult StudentView()
+        {
+            return View();
+        }
+        public async Task<IActionResult> StudentSearch(string searchString, string searchType)
+        {
+            var students = from v in context.Students select v;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                if(searchType == "nnumber")
+                {
+                    students = students.Where(s => s.nNumber.Contains(searchString));
+                }else if (searchType == "fname")
+                {
+                    students = students.Where(s => s.firstName.Contains(searchString));
+                }else if (searchType == "lname")
+                {
+                    students = students.Where(s => s.lastName.Contains(searchString));
+                }else if (searchType == "scholarship")
+                {
+                    students = students.Where(s => s.scholarship.Contains(searchString));
+                }else if (searchType == "year")
+                {
+                    students = students.Where(s => s.currentYear.Contains(searchString));
+                }else if (searchType == "age")
+                {
+                    students = students.Where(s => s.age.Contains(searchString));
+                }
+            }
+            return View(await students.ToListAsync());
+        }
+    }
+}
--- Donut_Deliverable1/Startup.cs
+++ Donut_Deliverable1/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Donut_Deliverable1.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace Donut_Deliverable1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("StudentDBConnection")));
+            services.AddTransient<IStudentRepository, EFStudentRepository>();
             services.AddControllersWithViews();
         }
 
