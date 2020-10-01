0dbfdbd467b45d8c17d8eeaede65fe0e1fda7c77
Charlie Tran <56985364+crimchar@users.noreply.github.com>
2020-10-01 13:28:18 -0400

    Simple admin model for login

---
+++ Donut_Deliverable1/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Donut_Deliverable1.Models
+{
+    public class Admin
+    {
+        public String userName { get; set; }
+        public String password { get; set; }
+    }
+}
