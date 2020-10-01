3bcd51255008242b726130aa206d3bbb33486ccd
Jackie Shrader <Jshrader1@hotmail.com>
2020-10-01 13:57:16 -0400

    Added simple check in page that logs time

---
+++ Donut_Deliverable1/Models/Attendance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Donut_Deliverable1.Models
+{
+    public class Attendance
+    {
+        public string NNumber { get; set; }
+
+        public DateTime GetDate { get; set; }
+    }
+
+
+}
