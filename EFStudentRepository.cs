eb09f7646056e68cd1140b810a1fb9bacd009600
John Lowery <59804669+johnslowery@users.noreply.github.com>
2020-12-08 13:18:34 -0500

    Fixed DataTable, added Create and View Students

---
+++ EFStudentRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Donut_Deliverable1.Models
+{
+    public class EFStudentRepository : IStudentRepository
+    {
+        private AppDbContext context;
+        private List<Student> _studentList;
+        public EFStudentRepository(AppDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IEnumerable<Student> Students
+        {
+            get
+            {
+                return context.Students;
+            }
+        }
+
+        public Student GetStudent(int Id)
+        {
+            _studentList = context.Students.ToList();
+            return _studentList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
---
+++ IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Donut_Deliverable1.Models
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> Students { get; }
+        Student GetStudent(int Id);
+    }
+}
