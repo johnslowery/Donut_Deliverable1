dde19f5b3f4aea8f8c1e84fe7b8432a16fe6f723
JohnSLowery <lowery.jack20@gmail.com>
2021-03-22 15:33:54 -0400

    Archive Students + Student Images
    Implemented ability to archive students and also give students images.

--- Donut_Deliverable1/wwwroot/js/student_table.js
+++ Donut_Deliverable1/wwwroot/js/student_table.js
@@ -1,20 +1,5 @@
 ﻿$(document).ready(function () {
     $("#student_table").DataTable({
-        data: data
+        
     });
 });
-
-var data = [
-    [
-        "John Lowery",
-        "n00000001",
-        "1/1/2000",
-        "McKay"
-    ],
-    [
-        "Other Person",
-        "n00000002",
-        "2/2/2000",
-        "Other Source"
-    ]
-]
