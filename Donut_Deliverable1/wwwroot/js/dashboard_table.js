22f0ee60d9d56dadfb8be32a477118347c77e120
Charlie Tran <56985364+crimchar@users.noreply.github.com>
2021-03-23 07:38:35 -0400

    charlie demo

--- Donut_Deliverable1/wwwroot/js/dashboard_table.js
+++ Donut_Deliverable1/wwwroot/js/dashboard_table.js
@@ -1,8 +1,32 @@
-﻿$(document).ready(function () {
+﻿var buttonCommon = {
+    exportOptions: {
+        columns: ':visible:not(.notexport)',
+        format: {
+            body: function (data, row, column, node) {
+                // if it is select
+                if (column == 4 || column == 5 || column == 6) {
+                    return $(data).find("option:selected").text()
+                }
+                else if(column == 7){
+                    return $(data).text()
+                }
+                else return data
+            }
+        }
+    }
+};
+
+$(document).ready(function () {
     $("#dashboard_table").DataTable({
         dom: 'Bfrtip',
         buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
+            'copy', $.extend(true, {}, buttonCommon, {
+                extend: "csv"
+            }), $.extend(true, {}, buttonCommon, {
+                extend: "excel"
+            }), $.extend(true, {}, buttonCommon, {
+                extend: "pdf"
+            })
         ]
     });
 });
