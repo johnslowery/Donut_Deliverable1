42b939b31a922e7e507a9a532f1e602dc07041fb
jackie shrader <Jshrader1@hotmail.com>
2021-03-22 20:49:09 -0400

    Fixing Attendance Routing Error

--- Donut_Deliverable1/Controllers/AttendanceController.cs
+++ Donut_Deliverable1/Controllers/AttendanceController.cs
@@ -82,7 +82,7 @@ namespace Donut_Deliverable1.Controllers
             DateTime lateTime = DateTime.Parse("2012/12/12 16:00:00.000");
 
 
-            SqlConnection con = new SqlConnection("Server=tcp:arcdb.database.windows.net,1433;Initial Catalog=arcDB;Persist Security Info=False;User ID=serveradmin;Password=#lL33tKarthik;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            SqlConnection con = new SqlConnection("Server=tcp:arcdb.database.windows.net,1433;Initial Catalog=arcDB;Persist Security Info=False;User ID=serveradmin;Password=#FakePassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             //SQL Command to add students check in time
             SqlCommand checkinset = new SqlCommand(@"UPDATE [dbo].[AttendanceLog] 
                 SET checkIn = GETDATE() 
